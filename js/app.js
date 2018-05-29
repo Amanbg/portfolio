8ab2421eb8e84d43b84b7332b5079d3e2a5c45f2
Amanbg <amanbabu8@gmail.com>
2018-05-29 16:45:24 +0530

    added smooth scroll

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-$(function() {
-    $('li').hide();
-});
+// $(function() {
+//     $('li').hide();
+// });
 $(window).on('scroll', function() {
     if ($(window).scrollTop()) {
         $('li').show();
         $('nav').addClass('black');
     } else {
-        $('li').hide();
+        // $('li').hide();
         $('nav').removeClass('black');
     }
 })
